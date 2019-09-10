931488b5e919c73d2dc27de616fb4222ce4abc33
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-10 22:28:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(integer)
+  divisors = []
+  counter = integer
+  while counter > 0 do
+    if integer % counter == 0
+      divisors << counter
+    end
+    counter -= 1
+  end
+  
+  if divisors.length == 2
+    return true
+  else
+    return false
+  end
+  
+end
